20df55e087712b9ef7a0ee7ca966ec45c24475ac
HandSomeCC <357737452@qq.com>
2018-06-29 16:01:28 +0800

    test

---
+++ src/main/java/com/cc/web/contorller/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.cc.web.contorller;
+
+import org.springframework.boot.SpringApplication;
+
+import com.cc.web.SpringbootThymeleafDemoApplication;
+
+/**
+ * @author CC
+ * @date 2018年6月29日
+ * @version 1.0
+ * @Description: 
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
